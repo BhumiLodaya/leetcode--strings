1f8ad9b6d14ea03eb2f0485bed4dcdb64f28a65a
BHUMI LODAYA <bhumilodaya23@gmail.com>
2025-10-08 18:43:12 +0530

    Time: 0 ms (100%), Space: 12.5 MB (10.1%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        result =""
+        while columnNumber>0:
+            columnNumber -=1
+            remainder = columnNumber%26
+            letter = chr(ord('A')+remainder)
+            result = letter +result 
+            columnNumber //=26
+        return result 
+        
+        
